0c2d8ba7b4415013a57394d5b86eb3523fad99b4
HariHarikrishnamoorthy <60098155+HariHarikrishnamoorthy@users.noreply.github.com>
2020-05-12 20:09:58 +0530

    This file is to Warp the picture

---
+++ WarpPrespective.py
@@ -0,0 +1,15 @@
+import cv2 
+import numpy as np
+
+img = cv2.imread('Resources\cards.jpg')
+
+width ,height = 250,350
+pts1 = np.float32([[111,219] , [287 ,188] ,[154 ,482] ,[352,440]])
+pts2 = np.float32([[0,0] ,[ width,0] ,[0,height] ,[width , height]])
+matrix = cv2.getPerspectiveTransform(pts1 ,pts2)
+img_out = cv2.warpPerspective(img , matrix ,(width , height))
+
+cv2.imshow("Image" , img)
+cv2.imshow("Output" , img_out)
+if cv2.waitKey(0)&0xFF == ord('q'):
+    cv2.destroyAllWindows()
